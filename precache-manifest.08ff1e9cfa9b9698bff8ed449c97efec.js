ad23dea50acfadd5d74850aa4d628e18c8b087f0
Pragati Chaturvedi <pragati.c810@gmail.com>
2023-02-15 23:15:30 +0530

    Updates

---
+++ precache-manifest.08ff1e9cfa9b9698bff8ed449c97efec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0b1c858882b6efc292c",
+    "url": "/chaos-game/static/css/main.7715e170.chunk.css"
+  },
+  {
+    "revision": "b0b1c858882b6efc292c",
+    "url": "/chaos-game/static/js/main.b0b1c858.chunk.js"
+  },
+  {
+    "revision": "7624ee30692a4b8c0362",
+    "url": "/chaos-game/static/js/1.7624ee30.chunk.js"
+  },
+  {
+    "revision": "d309870d936f4187c52e",
+    "url": "/chaos-game/static/js/runtime~main.d309870d.js"
+  },
+  {
+    "revision": "de6e3b5756b3c63c63a7ec96cdbaebc4",
+    "url": "/chaos-game/static/media/Hexagon.de6e3b57.png"
+  },
+  {
+    "revision": "70e9ead378840942538675450c1a4e83",
+    "url": "/chaos-game/static/media/Triangle.70e9ead3.png"
+  },
+  {
+    "revision": "c93c00be636a1dcc738560fcfa5bdb54",
+    "url": "/chaos-game/index.html"
+  }
+];
